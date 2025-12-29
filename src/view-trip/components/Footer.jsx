5029eb745c6ff06a3843f7e7007d9b279dcf70dd
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-12-29 23:39:36 +0530

    Update footer text with new creator information

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='my-7'>
-      <h2 className='text-center text-gray-400'>Created By Sonu Kumar Saw And Design by Priya Kumari  Ai Trip Planner</h2>
+      <h2 className='text-center text-gray-400'>Created By Kunal Raj Ai Trip Planner</h2>
     </div>
   )
 }
